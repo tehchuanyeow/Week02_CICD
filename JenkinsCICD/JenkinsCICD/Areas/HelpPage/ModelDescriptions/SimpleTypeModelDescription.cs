ba0feff4a686ca5c89eb9c4b693d4d141e6e47f3
Age <STUDENT@DESKTOP-G5ACJVG>
2018-08-06 14:28:29 +1000

    Initial Commit
    For Jenkins to Build its Pipelines.

---
+++ JenkinsCICD/JenkinsCICD/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace JenkinsCICD.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ JenkinsCICD/JenkinsCICD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JenkinsCICD.WebApiApplication" Language="C#" %>
